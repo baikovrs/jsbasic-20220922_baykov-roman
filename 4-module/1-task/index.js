77c58aec8ee7fb1b0f9a5eea5f8fe7c0a5a240a0
Байков Роман <baikov@infoculture.ru>
2023-04-26 13:14:07 +0300

    модуль 4 задача 1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,31 @@
-function makeFriendsList(friends) {
-  // ваш код...
+
+
+  let friends = [
+    {
+      firstName: 'Artsiom',
+      lastName: 'Mezin'
+    },  
+    {
+      firstName: 'Ilia',
+      lastName: 'Kantor'
+    },  
+    {
+      firstName: 'Christopher',
+      lastName: 'Michael'
+    }
+  ];
+
+function makeFriendsList (friends) {
+  const ul = document.createElement('ul');
+  friends.forEach(
+    (item) => {
+      const li = document.createElement('li');
+      li.appendChild(
+        document.createTextNode(`First: '${item.firstName}', Last: '${item.lastName}'`)
+        );
+      ul.appendChild(li);
+    }
+  );
+  document.body.appendChild(ul);
+  return ul;
 }
